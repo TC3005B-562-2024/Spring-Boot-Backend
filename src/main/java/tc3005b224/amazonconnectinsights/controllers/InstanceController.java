4f775cc95ff15d5e9eb729530e2b7c62a9b1cb99
Diego Jacobo Martínez <diegojmr5@gmail.com>
2024-04-07 14:11:04 -0600

    Added instance/ endpoint  The instance endpoint now returns the instance data with mocked values  closes #10

---
+++ src/main/java/tc3005b224/amazonconnectinsights/controllers/InstanceController.java
@@ -0,0 +1,51 @@
+package tc3005b224.amazonconnectinsights.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import tc3005b224.amazonconnectinsights.dto.InstanceDTO;
+
+@RestController
+@RequestMapping("/instance")
+public class InstanceController {
+
+    /**
+     * Get the Amazon Connect instance data
+     * 
+     * @return InstanceDTO
+     * @author Diego Jacobo Djmr5
+     * @see InstanceDTO
+     */
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found instance data", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = InstanceDTO.class))
+            }),
+            @ApiResponse(responseCode = "500", description = "Internal error", content = @Content),
+            @ApiResponse(responseCode = "503", description = "Couldn't connect to Amazon Connect API", content = @Content),
+    })
+    @Operation(summary = "Get the Amazon Connect instance data")
+    @GetMapping({ "/", "" })
+    public ResponseEntity<InstanceDTO> getInstanceData() {
+
+        InstanceDTO instance = new InstanceDTO(
+                "ARN1234",
+                "1",
+                "Call Center CDMX",
+                "https://amazonconnect.com/1",
+                "ACTIVE",
+                "Soporte",
+                false,
+                false,
+                false);
+
+        return ResponseEntity.ok(instance);
+    }
+
+}
---
+++ src/main/java/tc3005b224/amazonconnectinsights/dto/InstanceDTO.java
@@ -0,0 +1,115 @@
+package tc3005b224.amazonconnectinsights.dto;
+
+/**
+ * Data Transfer Object for Amazon Connect Instance entity information
+ * 
+ * @author Diego Jacobo Djmr5
+ */
+public class InstanceDTO {
+
+    /**
+     * Instance ARN - Amazon Resource Name (ARN) is a unique global identifier that
+     * identifies a resource
+     */
+    private String arn;
+    /**
+     * Instance ID - Unique identifier inside the Amazon Connect instance
+     */
+    private String id;
+    private String instanceAlias;
+    private String instanceAccessUrl;
+    private String instanceStatus;
+    private String serviceRole;
+    /**
+     * 
+     */
+    private boolean contactLensEnabled;
+    private boolean inboundCallsEnable;
+    private boolean outboundCallsEnable;
+
+    public String getArn() {
+        return arn;
+    }
+
+    public void setArn(String arn) {
+        this.arn = arn;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getInstanceAlias() {
+        return instanceAlias;
+    }
+
+    public void setInstanceAlias(String instanceAlias) {
+        this.instanceAlias = instanceAlias;
+    }
+
+    public String getInstanceAccessUrl() {
+        return instanceAccessUrl;
+    }
+
+    public void setInstanceAccessUrl(String instanceAccessUrl) {
+        this.instanceAccessUrl = instanceAccessUrl;
+    }
+
+    public String getInstanceStatus() {
+        return instanceStatus;
+    }
+
+    public void setInstanceStatus(String instanceStatus) {
+        this.instanceStatus = instanceStatus;
+    }
+
+    public String getServiceRole() {
+        return serviceRole;
+    }
+
+    public void setServiceRole(String serviceRole) {
+        this.serviceRole = serviceRole;
+    }
+
+    public boolean isContactLensEnabled() {
+        return contactLensEnabled;
+    }
+
+    public void setContactLensEnabled(boolean contactLensEnabled) {
+        this.contactLensEnabled = contactLensEnabled;
+    }
+
+    public boolean isInboundCallsEnable() {
+        return inboundCallsEnable;
+    }
+
+    public void setInboundCallsEnable(boolean inboundCallsEnable) {
+        this.inboundCallsEnable = inboundCallsEnable;
+    }
+
+    public boolean isOutboundCallsEnable() {
+        return outboundCallsEnable;
+    }
+
+    public void setOutboundCallsEnable(boolean outboundCallsEnable) {
+        this.outboundCallsEnable = outboundCallsEnable;
+    }
+
+    public InstanceDTO(String arn, String id, String instanceAlias, String instanceAccessUrl, String instanceStatus,
+            String serviceRole, boolean contactLensEnabled, boolean inboundCallsEnable, boolean outboundCallsEnable) {
+        this.arn = arn;
+        this.id = id;
+        this.instanceAlias = instanceAlias;
+        this.instanceAccessUrl = instanceAccessUrl;
+        this.instanceStatus = instanceStatus;
+        this.serviceRole = serviceRole;
+        this.contactLensEnabled = contactLensEnabled;
+        this.inboundCallsEnable = inboundCallsEnable;
+        this.outboundCallsEnable = outboundCallsEnable;
+    }
+
+}
