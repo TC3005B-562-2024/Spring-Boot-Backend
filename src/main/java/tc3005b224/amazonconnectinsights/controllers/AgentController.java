8d6ec4fb016960492d891e36fedcd4787c38ae72
MoisesAdame <moisesadame01@gmail.com>
2024-04-07 14:01:38 -0600

    Documentation of /agent/{id}/transfer

--- src/main/java/tc3005b224/amazonconnectinsights/controllers/AgentController.java
+++ src/main/java/tc3005b224/amazonconnectinsights/controllers/AgentController.java
@@ -1,5 +1,10 @@
 package tc3005b224.amazonconnectinsights.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import tc3005b224.amazonconnectinsights.dto.skill.SkillDataDTO;
@@ -8,6 +13,11 @@ import tc3005b224.amazonconnectinsights.dto.skill.SkillDataDTO;
 @RestController
 @RequestMapping("/agents")
 public class AgentController {
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Agent properly transferred.", content = @Content),
+            @ApiResponse(responseCode = "400", description = "Invalid action.", content = @Content),
+    })
+    @Operation(summary = "Transfer an agent to a desired skill.")
     @PostMapping("/{id}/transfer")
     public ResponseEntity<String> transfer(@RequestBody SkillDataDTO skill, @PathVariable("id") int id)  {
         // TODO: Call API that shows status of the agent with the specified id.
