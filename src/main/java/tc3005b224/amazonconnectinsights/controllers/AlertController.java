4a9f20bfd5230e89b5a6670728f5698d11e52fcf
Alfredo-Vqz <a01656384@tec.mx>
2024-04-08 01:20:58 -0600

    Creación enpoint GET alert_id
    Se creo el enpoint GET que da la información unicamente del enpoint con el "alert_id" que se indica

--- src/main/java/tc3005b224/amazonconnectinsights/controllers/AlertController.java
+++ src/main/java/tc3005b224/amazonconnectinsights/controllers/AlertController.java
@@ -5,10 +5,12 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import tc3005b224.amazonconnectinsights.dto.alerts.AlertDTO;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/alerts")
@@ -27,5 +29,20 @@ public class AlertController {
 
         String result = String.format("Alert %d ignored.", id);
         return ResponseEntity.ok(result);
+    }
+
+    @GetMapping("/{alert_id}")
+    public ResponseEntity<Optional<AlertDTO>> getAlert(@PathVariable("alert_id") String alert_id){
+        List<AlertDTO> alerts = Arrays.asList(
+                new AlertDTO("1", "El cliente se desconecto", "critica", "agent1", "skill1", "queue1", "contact1"),
+                new AlertDTO("2", "Sistema en riesgo de sobrecarga", "alta", "agent2", "skill2", "queue2", "contact2"),
+                new AlertDTO("3", "Error de conexión", "critica", "agent3", "skill3", "queue3", "contact3")
+        );
+
+        Optional<AlertDTO> criticalAlerts = alerts.stream()
+                .filter(alert -> alert.getId().equals(alert_id))
+                .findFirst();
+
+        return ResponseEntity.ok(criticalAlerts);
     }
 }
