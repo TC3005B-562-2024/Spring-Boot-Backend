12ff56fd75627233500cf0e65b543342825253f7
Diego Jacobo Martínez <diegojmr5@gmail.com>
2024-04-07 21:12:56 -0600

    Training filtering
     Now you can filter the trainings by adding the query
     parameter agentId.
    
     Example:
     http://localhost:8080/trainings?agentId=2
    
     closes #48

--- src/main/java/tc3005b224/amazonconnectinsights/controllers/TrainingController.java
+++ src/main/java/tc3005b224/amazonconnectinsights/controllers/TrainingController.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -16,9 +17,8 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import tc3005b224.amazonconnectinsights.dto.training.TrainingDTO;
 
-
 @RestController
-@RequestMapping("training")
+@RequestMapping("trainings")
 public class TrainingController {
 
     /**
@@ -26,6 +26,7 @@ public class TrainingController {
      * 
      * TODO: Create training model in db.
      * 
+     * @param agentId
      * @return List<TrainingDTO>
      * @author Diego Jacobo Djmr5
      * @see TrainingDTO
@@ -38,15 +39,28 @@ public class TrainingController {
             @ApiResponse(responseCode = "503", description = "Couldn't connect to Database", content = @Content),
     })
     @Operation(summary = "Get the list of all trainings data")
-    @GetMapping("/")
-    public ResponseEntity<List<TrainingDTO>> getTrainingsData() {
+    @GetMapping("")
+    public ResponseEntity<List<TrainingDTO>> getTrainingsData(
+            @RequestParam(required = false, name = "agentId") String agentId) {
 
         List<TrainingDTO> trainings = new ArrayList<TrainingDTO>();
         Date creationDate = new Date();
         Date completionDate = new Date();
         trainings.add(new TrainingDTO("1", "This is a sample description", "1", creationDate, completionDate, false));
         trainings.add(new TrainingDTO("2", "This is a sample description", "2", creationDate, completionDate, true));
-        trainings.add(new TrainingDTO("3", "This is a sample description", "3", creationDate, completionDate, false));
+        trainings.add(new TrainingDTO("3", "This is a sample description", "2", creationDate, completionDate, false));
+        trainings.add(new TrainingDTO("4", "This is a sample description", "3", creationDate, completionDate, false));
+
+        // Filter by agentId
+        if (agentId != null) {
+            List<TrainingDTO> filteredTrainings = new ArrayList<TrainingDTO>();
+            for (TrainingDTO training : trainings) {
+                if (training.getAgentId().equals(agentId)) {
+                    filteredTrainings.add(training);
+                }
+            }
+            return ResponseEntity.ok(filteredTrainings);
+        }
 
         return ResponseEntity.ok(trainings);
     }
