0c54391f25abbf5d333db41751ac8bb486eca87c
ArturoCarballoArias <a01662245@tec.mx>
2024-04-08 00:26:59 -0600

    Get y Delete
    GET
    /alerts/all
    
    DELETE
    /alerts/{alert_id}

--- src/main/java/tc3005b224/amazonconnectinsights/controllers/AlertController.java
+++ src/main/java/tc3005b224/amazonconnectinsights/controllers/AlertController.java
@@ -5,10 +5,11 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import tc3005b224.amazonconnectinsights.dto.Alerts.AlertAllDTO;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/alerts")
@@ -17,6 +18,35 @@ public class AlertController {
             @ApiResponse(responseCode = "200", description = "Alert properly ignored.", content = @Content),
             @ApiResponse(responseCode = "400", description = "Invalid action.", content = @Content),
     })
+
+    @GetMapping("/all")
+    public List<AlertAllDTO> getAllAlerts() {
+        AlertAllDTO Alert = new AlertAllDTO();
+        Alert.setId("1");
+        Alert.setDescription("El cliente se enfurecio");
+        Alert.setPriority("Critica");
+        Alert.setAgentId("agent1");
+        Alert.setSkillId("skill1");
+        Alert.setQueueId("queue1");
+        Alert.setContactId("contact1");
+
+        AlertAllDTO Alert2 = new AlertAllDTO();
+        Alert2.setId("2");
+        Alert2.setDescription("El cliente se enfurecio mucho");
+        Alert2.setPriority("Critica");
+        Alert2.setAgentId("agent1");
+        Alert2.setSkillId("skill1");
+        Alert2.setQueueId("queue1");
+        Alert2.setContactId("contact1");
+
+        return Arrays.asList(Alert, Alert2);
+    }
+
+    @DeleteMapping("/{alert_id}")
+    public ResponseEntity<String> deleteAlert(@PathVariable("alert_id") String alertId) {
+        return ResponseEntity.ok("Alert " + alertId + " was deleted");
+    }
+
     @Operation(summary = "Ignore a desired alert.")
     @PostMapping("/{id}/ignore")
     public ResponseEntity<String> ignore(@PathVariable("id") int id){
