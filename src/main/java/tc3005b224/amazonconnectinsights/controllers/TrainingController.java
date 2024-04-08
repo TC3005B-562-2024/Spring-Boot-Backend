80c1bccbac78155d309f9939e6842a914920e0c7
Diego Jacobo Martínez <85468901+Djmr5@users.noreply.github.com>
2024-04-07 20:30:52 -0600

    List Trainings endpoint (#45)
    * Treining Controller file
    
    * List Trainings endpoints
    
     Endpoint that returns a list of all trainings
     closes #35

---
+++ src/main/java/tc3005b224/amazonconnectinsights/controllers/TrainingController.java
@@ -0,0 +1,54 @@
+package tc3005b224.amazonconnectinsights.controllers;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import tc3005b224.amazonconnectinsights.dto.training.TrainingDTO;
+
+
+@RestController
+@RequestMapping("training")
+public class TrainingController {
+
+    /**
+     * Get the list of all trainings data
+     * 
+     * TODO: Create training model in db.
+     * 
+     * @return List<TrainingDTO>
+     * @author Diego Jacobo Djmr5
+     * @see TrainingDTO
+     */
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found trainings data", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = TrainingDTO.class))
+            }),
+            @ApiResponse(responseCode = "500", description = "Internal error", content = @Content),
+            @ApiResponse(responseCode = "503", description = "Couldn't connect to Database", content = @Content),
+    })
+    @Operation(summary = "Get the list of all trainings data")
+    @GetMapping("/")
+    public ResponseEntity<List<TrainingDTO>> getTrainingsData() {
+
+        List<TrainingDTO> trainings = new ArrayList<TrainingDTO>();
+        Date creationDate = new Date();
+        Date completionDate = new Date();
+        trainings.add(new TrainingDTO("1", "This is a sample description", "1", creationDate, completionDate, false));
+        trainings.add(new TrainingDTO("2", "This is a sample description", "2", creationDate, completionDate, true));
+        trainings.add(new TrainingDTO("3", "This is a sample description", "3", creationDate, completionDate, false));
+
+        return ResponseEntity.ok(trainings);
+    }
+
+}
