b830c18e4b71df9ba671a3ab1770db33e8da0420
Diego Jacobo Martínez <85468901+Djmr5@users.noreply.github.com>
2024-04-09 15:18:58 -0600

    Training filtering (#50)
    * Training filtering
    
     Now you can filter the trainings by adding the query
     parameter agentId.
    
     Example:
     http://localhost:8080/trainings?agentId=2
    
     closes #48
    
    * Added a ListTrainingDTO

--- src/main/java/tc3005b224/amazonconnectinsights/controllers/TrainingController.java
+++ src/main/java/tc3005b224/amazonconnectinsights/controllers/TrainingController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -15,11 +16,11 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import tc3005b224.amazonconnectinsights.dto.training.ListTrainingDTO;
 import tc3005b224.amazonconnectinsights.dto.training.TrainingDTO;
 
-
 @RestController
-@RequestMapping("training")
+@RequestMapping("trainings")
 public class TrainingController {
 
     /**
@@ -27,29 +28,43 @@ public class TrainingController {
      * 
      * TODO: Create training model in db.
      * 
-     * @return List<TrainingDTO>
+     * @param agentId
+     * @return ListTrainingDTO
      * @author Diego Jacobo Djmr5
      * @see TrainingDTO
      */
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Found trainings data", content = {
-                    @Content(mediaType = "application/json", schema = @Schema(implementation = TrainingDTO.class))
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = ListTrainingDTO.class))
             }),
             @ApiResponse(responseCode = "500", description = "Internal error", content = @Content),
             @ApiResponse(responseCode = "503", description = "Couldn't connect to Database", content = @Content),
     })
     @Operation(summary = "Get the list of all trainings data")
-    @GetMapping("/")
-    public ResponseEntity<List<TrainingDTO>> getTrainingsData() {
+    @GetMapping("")
+    public ResponseEntity<ListTrainingDTO> getTrainingsData(
+            @RequestParam(required = false, name = "agentId") String agentId) {
 
         List<TrainingDTO> trainings = new ArrayList<TrainingDTO>();
         Date creationDate = new Date();
         Date completionDate = new Date();
         trainings.add(new TrainingDTO("1", "This is a sample description", "1", creationDate, completionDate, false));
         trainings.add(new TrainingDTO("2", "This is a sample description", "2", creationDate, completionDate, true));
-        trainings.add(new TrainingDTO("3", "This is a sample description", "3", creationDate, completionDate, false));
+        trainings.add(new TrainingDTO("3", "This is a sample description", "2", creationDate, completionDate, false));
+        trainings.add(new TrainingDTO("4", "This is a sample description", "3", creationDate, completionDate, false));
 
-        return ResponseEntity.ok(trainings);
+        // Filter by agentId
+        if (agentId != null) {
+            List<TrainingDTO> filteredTrainings = new ArrayList<TrainingDTO>();
+            for (TrainingDTO training : trainings) {
+                if (training.getAgentId().equals(agentId)) {
+                    filteredTrainings.add(training);
+                }
+            }
+            return ResponseEntity.ok(new ListTrainingDTO(filteredTrainings));
+        }
+
+        return ResponseEntity.ok(new ListTrainingDTO(trainings));
     }
 
     /**
