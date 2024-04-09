8062f3b9acac198651ae9c98d93d56a0b97e9880
Diego Jacobo Martínez <diegojmr5@gmail.com>
2024-04-08 19:03:59 -0600

    Update ApiResponses and request body

--- src/main/java/tc3005b224/amazonconnectinsights/controllers/interveneController.java
+++ src/main/java/tc3005b224/amazonconnectinsights/controllers/interveneController.java
@@ -9,27 +9,22 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import tc3005b224.amazonconnectinsights.dto.intervene.interveneDTO;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @RestController
 @RequestMapping("/intervene")
 public class interveneController {
 
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Found the timeline", content = {
+            @ApiResponse(responseCode = "200", description = "Intervention in progress", content = {
                     @Content(mediaType = "application/json", schema = @Schema(implementation = interveneDTO.class))
             }),
-            @ApiResponse(responseCode = "404", description = "Timeline not found", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Agent not found", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal error", content = @Content),
+            @ApiResponse(responseCode = "503", description = "Couldn't connect to Amazon Connect API", content = @Content),
     })
-    @Operation(summary = "Get all posts for a user")
+    @Operation(summary = "Intervene in a contact")
     @PostMapping("/")
-    public ResponseEntity<List<interveneDTO>> PostPosts() {
-        List<interveneDTO> result = new ArrayList<>();
-        interveneDTO post1 = new interveneDTO();
-        post1.setAgent_id("123"); // Sample agent id
-        result.add(post1);
-        return ResponseEntity.ok(result);
+    public ResponseEntity<interveneDTO> PostPosts(@RequestBody interveneDTO intervene) {
+        return ResponseEntity.ok(intervene);
     }
 
 }
