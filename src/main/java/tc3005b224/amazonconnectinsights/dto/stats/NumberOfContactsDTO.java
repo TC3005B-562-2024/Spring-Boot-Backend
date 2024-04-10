5463a36c68a955e6688547649157b04fb18f2dd8
SirPotato28 <A01662814@tec.mx>
2024-04-09 18:19:23 -0600

    Advance

--- src/main/java/tc3005b224/amazonconnectinsights/dto/stats/NumberOfContactsDTO.java
+++ src/main/java/tc3005b224/amazonconnectinsights/dto/stats/NumberOfContactsDTO.java
@@ -22,28 +22,3 @@ public class NumberOfContactsDTO {
         this.timeWindow = timeWindow;
     }
 }
-
-package tc3005b224.amazonconnectinsights.dto.stats;
-
-import java.util.Date;
-
-public class TimeWindowDTO {
-    private Date start;
-    private Date end;
-
-    public Date getStart() {
-        return start;
-    }
-
-    public void setStart(Date start) {
-        this.start = start;
-    }
-
-    public Date getEnd() {
-        return end;
-    }
-
-    public void setEnd(Date end) {
-        this.end = end;
-    }
-}
