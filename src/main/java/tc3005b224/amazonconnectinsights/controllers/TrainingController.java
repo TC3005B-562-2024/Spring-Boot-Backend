dad8cf484f515e96eb8a93fdf97b0f12bcc11fdb
Diego Jacobo Martínez <diegojmr5@gmail.com>
2024-04-27 15:31:37 -0600

    Trainings endpoints and filters

--- src/main/java/tc3005b224/amazonconnectinsights/controllers/TrainingController.java
+++ src/main/java/tc3005b224/amazonconnectinsights/controllers/TrainingController.java
@@ -1,92 +1,148 @@
 package tc3005b224.amazonconnectinsights.controllers;
 
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
+import org.apache.coyote.BadRequestException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import tc3005b224.amazonconnectinsights.dto.training.ListTrainingDTO;
+import tc3005b224.amazonconnectinsights.dto.filter.FilterDTO;
 import tc3005b224.amazonconnectinsights.dto.training.TrainingDTO;
+import tc3005b224.amazonconnectinsights.dto.training.TrainingNoIdDTO;
+import tc3005b224.amazonconnectinsights.service.TrainingsService;
 
 @RestController
 @RequestMapping("trainings")
 public class TrainingController {
 
-    /**
-     * Get the list of all trainings data
-     * 
-     * TODO: Create training model in db.
-     * 
-     * @param agentId
-     * @return ListTrainingDTO
-     * @author Diego Jacobo Djmr5
-     * @see TrainingDTO
-     */
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Found trainings data", content = {
-                    @Content(mediaType = "application/json", schema = @Schema(implementation = ListTrainingDTO.class))
-            }),
-            @ApiResponse(responseCode = "500", description = "Internal error", content = @Content),
-            @ApiResponse(responseCode = "503", description = "Couldn't connect to Database", content = @Content),
-    })
-    @Operation(summary = "Get the list of all trainings data")
-    @GetMapping("")
-    public ResponseEntity<ListTrainingDTO> getTrainingsData(
-            @RequestParam(required = false, name = "agentId") String agentId) {
+        @Autowired
+        private TrainingsService trainingService;
 
-        List<TrainingDTO> trainings = new ArrayList<TrainingDTO>();
-        Date creationDate = new Date();
-        Date completionDate = new Date();
-        trainings.add(new TrainingDTO("1", "This is a sample description", "1", creationDate, completionDate, false));
-        trainings.add(new TrainingDTO("2", "This is a sample description", "2", creationDate, completionDate, true));
-        trainings.add(new TrainingDTO("3", "This is a sample description", "2", creationDate, completionDate, false));
-        trainings.add(new TrainingDTO("4", "This is a sample description", "3", creationDate, completionDate, false));
-
-        // Filter by agentId
-        if (agentId != null) {
-            List<TrainingDTO> filteredTrainings = new ArrayList<TrainingDTO>();
-            for (TrainingDTO training : trainings) {
-                if (training.getAgentId().equals(agentId)) {
-                    filteredTrainings.add(training);
+        /**
+         * Get the list of all trainings data
+         * 
+         * @param agentId
+         * @return ListTrainingDTO
+         * @author Diego Jacobo Djmr5
+         * @throws BadRequestException
+         * @see TrainingDTO
+         */
+        @ApiResponses(value = {
+                        @ApiResponse(responseCode = "200", description = "Found trainings data", content = {
+                                        @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = TrainingDTO.class)))
+                        }),
+                        @ApiResponse(responseCode = "400", description = "Invalid filters or Too many filters", content = @Content),
+                        @ApiResponse(responseCode = "500", description = "Internal error", content = @Content),
+                        @ApiResponse(responseCode = "503", description = "Couldn't connect to Database", content = @Content),
+        })
+        @Operation(summary = "Get the list of all trainings data")
+        @PostMapping("")
+        public ResponseEntity<?> getTrainingsData(
+                        @RequestBody(required = false) List<FilterDTO> filters) throws BadRequestException {
+                try {
+                        return ResponseEntity.ok(trainingService.findAll(filters));
+                } catch (Exception e) {
+                        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
                 }
-            }
-            return ResponseEntity.ok(new ListTrainingDTO(filteredTrainings));
         }
 
-        return ResponseEntity.ok(new ListTrainingDTO(trainings));
-    }
+        /**
+         * Get the training data by its id
+         * 
+         * @param trainingId
+         * @return TrainingDTO
+         * @author Diego Jacobo Djmr5
+         * @see TrainingDTO
+         */
+        @ApiResponses(value = {
+                        @ApiResponse(responseCode = "200", description = "Found training data", content = {
+                                        @Content(mediaType = "application/json", schema = @Schema(implementation = TrainingDTO.class))
+                        }),
+                        @ApiResponse(responseCode = "404", description = "Training not found", content = @Content),
+                        @ApiResponse(responseCode = "500", description = "Internal error", content = @Content),
+                        @ApiResponse(responseCode = "503", description = "Couldn't connect to Database", content = @Content),
+        })
+        @Operation(summary = "Get the training data by its id")
+        @GetMapping("{trainingId}")
+        public ResponseEntity<TrainingDTO> getTrainingData(@PathVariable("trainingId") Long trainingId) {
+                return ResponseEntity.ok(trainingService.findById(trainingId));
+        }
 
-    /**
-     * Get the training data by its id
-     * 
-     * @param trainingId
-     * @return TrainingDTO
-     * @author Diego Jacobo Djmr5
-     * @see TrainingDTO
-     */
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Found training data", content = {
-                    @Content(mediaType = "application/json", schema = @Schema(implementation = TrainingDTO.class))
-            }),
-            @ApiResponse(responseCode = "500", description = "Internal error", content = @Content),
-            @ApiResponse(responseCode = "503", description = "Couldn't connect to Database", content = @Content),
-    })
-    @Operation(summary = "Get the training data by its id")
-    @GetMapping("{trainingId}")
-    public ResponseEntity<TrainingDTO> getTrainingData(@PathVariable("trainingId") String trainingId) {
-        return ResponseEntity
-                .ok(new TrainingDTO(trainingId, "This is a sample description", "1", new Date(), new Date(), false));
-    }
+        /**
+         * Save a new training data
+         * 
+         * @param newTraining
+         * @return TrainingDTO
+         * @author Diego Jacobo Djmr5
+         * @see TrainingDTO
+         */
+        @Operation(summary = "Save a new training data", responses = {
+                        @ApiResponse(responseCode = "200", description = "Training data saved", content = {
+                                        @Content(mediaType = "application/json", schema = @Schema(implementation = TrainingDTO.class))
+                        }),
+                        @ApiResponse(responseCode = "400", description = "Invalid atributtes", content = @Content),
+                        @ApiResponse(responseCode = "500", description = "Internal error", content = @Content),
+                        @ApiResponse(responseCode = "503", description = "Couldn't connect to Database", content = @Content),
+        })
+        @PostMapping("/create")
+        public ResponseEntity<TrainingDTO> saveTrainingData(@RequestBody TrainingNoIdDTO newTraining) {
+                return ResponseEntity.ok(trainingService.saveTraining(null, newTraining));
+        }
 
+        /**
+         * Update a training data by its id
+         * 
+         * @param trainingId
+         * @return
+         * @author Diego Jacobo Djmr5
+         * @see TrainingDTO
+         */
+        @Operation(summary = "Update a training data by its id", responses = {
+                        @ApiResponse(responseCode = "200", description = "Training data updated", content = {
+                                        @Content(mediaType = "application/json", schema = @Schema(implementation = TrainingDTO.class))
+                        }),
+                        @ApiResponse(responseCode = "404", description = "Training not found", content = @Content),
+                        @ApiResponse(responseCode = "500", description = "Internal error", content = @Content),
+                        @ApiResponse(responseCode = "503", description = "Couldn't connect to Database", content = @Content),
+        })
+        @PutMapping("{trainingId}")
+        public ResponseEntity<TrainingDTO> updateTrainingData(@PathVariable("trainingId") Short trainingId,
+                        @RequestBody TrainingNoIdDTO newTraining) {
+                return ResponseEntity.ok(trainingService.saveTraining(trainingId, newTraining));
+        }
+
+        /**
+         * Delete a training data by its id
+         * 
+         * @param trainingId
+         * @return void
+         * @author Diego Jacobo Djmr5
+         * @see TrainingDTO
+         */
+        @Operation(summary = "Delete a training data by its id", responses = {
+                        @ApiResponse(responseCode = "204", description = "Training data deleted", content = @Content),
+                        @ApiResponse(responseCode = "404", description = "Training not found", content = @Content),
+                        @ApiResponse(responseCode = "500", description = "Internal error", content = @Content),
+                        @ApiResponse(responseCode = "503", description = "Couldn't connect to Database", content = @Content),
+        })
+        @DeleteMapping("{trainingId}")
+        public ResponseEntity<Void> deleteTrainingData(@PathVariable("trainingId") Long trainingId) {
+                trainingService.deleteTraining(trainingId);
+                return ResponseEntity.noContent().build();
+        }
 }
