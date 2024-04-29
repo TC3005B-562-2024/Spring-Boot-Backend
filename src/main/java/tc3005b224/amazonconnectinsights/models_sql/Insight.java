26dc7b341860684ef929a6fcad0bc9a2c0269c39
MoisesAdame <moisesadame01@gmail.com>
2024-04-29 12:54:26 -0600

    Fixing building errors.

--- src/main/java/tc3005b224/amazonconnectinsights/models_sql/Insight.java
+++ src/main/java/tc3005b224/amazonconnectinsights/models_sql/Insight.java
@@ -58,6 +58,7 @@ public class Insight {
         this.dateRegistered = new Date();
         this.dateUpdated = new Date();
         this.isActive = insightDTO.getActive();
+    }
 
     public void updateFromDTO(InsightDTO insightDTO){
         this.dateUpdated = new Date();
