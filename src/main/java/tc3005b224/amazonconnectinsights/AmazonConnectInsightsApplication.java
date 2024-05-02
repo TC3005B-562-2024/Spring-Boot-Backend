4ac254d27e53e2af09d7a1d9043b17f79611abbf
Diego Jacobo Martínez <diegojmr5@gmail.com>
2024-05-02 15:33:31 -0600

    CORS

--- src/main/java/tc3005b224/amazonconnectinsights/AmazonConnectInsightsApplication.java
+++ src/main/java/tc3005b224/amazonconnectinsights/AmazonConnectInsightsApplication.java
@@ -3,8 +3,9 @@ package tc3005b224.amazonconnectinsights;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class AmazonConnectInsightsApplication {
@@ -14,13 +15,14 @@ public class AmazonConnectInsightsApplication {
 	}
 
 	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**");
-			}
-		};
+	public CorsFilter corsConfigurer() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.addAllowedOrigin("*"); // Allow requests from any origin
+		config.addAllowedHeader("*"); // Allow all headers
+		config.addAllowedMethod("*"); // Allow all HTTP methods
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
 	}
 
 }
