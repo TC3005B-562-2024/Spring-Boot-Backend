3e250ea77e7ce4966d0fd296af463ead98da53a0
Diego Jacobo Martínez <85468901+Djmr5@users.noreply.github.com>
2024-05-15 18:43:07 -0600

    Reorganized and removed unused imports (#111)

--- src/main/java/tc3005b224/amazonconnectinsights/repository/AlertRepository.java
+++ src/main/java/tc3005b224/amazonconnectinsights/repository/AlertRepository.java
@@ -1,11 +1,12 @@
 package tc3005b224.amazonconnectinsights.repository;
 
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.repository.CrudRepository;
-import tc3005b224.amazonconnectinsights.models_sql.Alert;
-
 import java.util.List;
 import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.CrudRepository;
+
+import tc3005b224.amazonconnectinsights.models_sql.Alert;
 
 public interface AlertRepository extends CrudRepository<Alert, Long>, JpaSpecificationExecutor<Alert> {
     List<Alert> findByConnectionIdentifierAndResourceContainingAndSolvedAndInsight_Category_PriorityAndInsight_Category_DenominationContaining(int connectionIdentifier, String resource, Boolean solved, int priority, String denomination);
