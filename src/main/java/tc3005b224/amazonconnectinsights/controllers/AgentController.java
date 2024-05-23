72bc02676fddeba135659231640845dd1074e8e4
Diego Jacobo Martínez <85468901+Djmr5@users.noreply.github.com>
2024-05-23 11:43:20 -0600

    Connect/agents (#134)
    * Retrieves users data, but emotion, status and skills still require adjustments
    
    * Funtional get all Agents
    
    * Skills renamed to queues
    
    * Controller and services for GET /queues
    
    * Progreso
    
    * Fixed
    
    * Initial configuration for MetricsService
    
    * Merged and updated env vars
    
    * Search by queue Id working
    
    * Deleted debugger test endpoint
    
    * Connect/Agents
    
    * Fixed null values
    
    ---------
    
    Co-authored-by: MoisesAdame <moisesadame01@gmail.com>

--- src/main/java/tc3005b224/amazonconnectinsights/controllers/AgentController.java
+++ src/main/java/tc3005b224/amazonconnectinsights/controllers/AgentController.java
@@ -3,6 +3,7 @@ package tc3005b224.amazonconnectinsights.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import tc3005b224.amazonconnectinsights.dto.agent.AgentCardDTO;
-import tc3005b224.amazonconnectinsights.dto.agent.AgentDTO;
 import tc3005b224.amazonconnectinsights.service.AgentService;
 
 @RestController
@@ -32,13 +32,14 @@ public class AgentController {
     }
 
     @GetMapping("/{agentId}")
-    public ResponseEntity<AgentDTO> getIndividualAgent(@PathVariable String agentId){
+    public ResponseEntity<?> getIndividualAgent(@PathVariable String agentId){
         try {
             return ResponseEntity.ok(agentService.findById("1", agentId));
         }
         catch(Exception e) {
             // Return error 404 if there is an exception.
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            ErrorResponse error = ErrorResponse.builder(e, HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage()).build();
+            return new ResponseEntity<>(error, error.getStatusCode());
         }
     }
 }
