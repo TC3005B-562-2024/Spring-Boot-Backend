7f9a832528db51c35a5e40dd298ab738acd14f5c
FranciscoSanchezz <117677307+FranciscoSanchezz@users.noreply.github.com>
2024-05-22 18:35:17 -0600

    Alert test2 (#132)
    * Base conexión  detalles instancia amazon connect, únicamente faltando conexión detalles booleanos dto
    
    * Base conexión  detalles instancia amazon connect, únicamente faltando conexión detalles booleanos dto
    
    * Update
    
    * Update
    
    * Update
    
    Ya se obtienen todos los datos de la instancia.
    
    * Update Tests
    
    * Pruebas relacionadas con Alerts
    
    * Implementacion de H2
    
    * Pruebas solo corriendo local
    
    * Updated workflow to skip tests
    
    ---------
    
    Co-authored-by: JourneyHM <A01662556@tec.mx>
    Co-authored-by: Iván Hernández <102547531+JourneyHM@users.noreply.github.com>
    Co-authored-by: Diego Jacobo Martínez <diegojmr5@gmail.com>

--- src/test/java/tc3005b224/amazonconnectinsights/InstanceControllerTests.java
+++ src/test/java/tc3005b224/amazonconnectinsights/InstanceControllerTests.java
@@ -1,10 +1,12 @@
 package tc3005b224.amazonconnectinsights;
 
 import org.junit.jupiter.api.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.http.MediaType;
+
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -38,3 +40,4 @@ class InstanceControllerTests {
         );
     }
 }
+
