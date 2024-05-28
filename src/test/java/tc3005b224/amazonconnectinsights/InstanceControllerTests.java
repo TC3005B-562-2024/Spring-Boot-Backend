02c903c95002b59fa8455e21598e3fad3c8831b6
JourneyHM <A01662556@tec.mx>
2024-05-27 21:32:27 -0600

    Implementación de tests funcionales
    Para comprobar el correcto funcionamiento de la obtención de datos a partir de la conexión con la instancia de Amazon Connect

--- src/test/java/tc3005b224/amazonconnectinsights/InstanceControllerTests.java
+++ src/test/java/tc3005b224/amazonconnectinsights/InstanceControllerTests.java
@@ -1,5 +1,11 @@
 package tc3005b224.amazonconnectinsights;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +17,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.io.IOException;
+
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 @SpringBootTest
@@ -19,11 +27,47 @@ class InstanceControllerTests {
     @Autowired
     MockMvc mockMvc;
 
+    private String obtainAuthToken() throws IOException {
+        OkHttpClient client = new OkHttpClient();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = objectMapper.writeValueAsString(new AlertTests.AuthRequest("test@g.com", "123456", true));
+
+        RequestBody body = RequestBody.create(json, okhttp3.MediaType.get("application/json; charset=utf-8"));
+        Request request = new Request.Builder()
+                .url("https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyB1NzTkPKLQW-aHJrHwVUKjOVY209urse4")
+                .post(body)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            if (response.isSuccessful() && response.body() != null) {
+                String responseBody = response.body().string();
+                JsonNode jsonNode = objectMapper.readTree(responseBody);
+                return jsonNode.path("idToken").asText();
+            } else {
+                throw new IOException("Unexpected code " + response);
+            }
+        }
+    }
+
+    static class AuthRequest {
+        public String email;
+        public String password;
+        public boolean returnSecureToken;
+
+        public AuthRequest(String email, String password, boolean returnSecureToken) {
+            this.email = email;
+            this.password = password;
+            this.returnSecureToken = returnSecureToken;
+        }
+    }
+
     @Test
     public void getInstanceDetailsTest() throws Exception {
         String tokenValue = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyLCJyb2xlIjoiYWdlbnQiLCJpbnN0YW5jZUlkIjoieW91ci1pbnN0YW5jZS1pZCJ9.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
         mockMvc.perform(
             MockMvcRequestBuilders.get("/instance?token=" + tokenValue)
+                    .header("Authorization", "Bearer " + obtainAuthToken())
             .contentType(MediaType.APPLICATION_JSON))
             .andExpect(MockMvcResultMatchers.status().isOk())
             .andDo(print()
@@ -34,6 +78,7 @@ class InstanceControllerTests {
     public void tokenExceptionGetInstanceDetailsTest()throws Exception {
         mockMvc.perform(
             MockMvcRequestBuilders.get("/instance")
+                    .header("Authorization", "Bearer " + obtainAuthToken())
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isBadRequest())
                 .andDo(print()
