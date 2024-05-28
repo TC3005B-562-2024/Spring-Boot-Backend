d0d84e4056a09b533557e5c074c624b37e798db7
Diego Jacobo Martínez <diegojmr5@gmail.com>
2024-05-27 19:50:47 -0600

    Fixed circular dependency

---
+++ src/main/java/tc3005b224/amazonconnectinsights/service/RoutingProfileService.java
@@ -0,0 +1,26 @@
+package tc3005b224.amazonconnectinsights.service;
+
+import java.util.List;
+
+import software.amazon.awssdk.services.connect.model.ListRoutingProfilesRequest;
+import software.amazon.awssdk.services.connect.model.ListRoutingProfilesResponse;
+import software.amazon.awssdk.services.connect.model.RoutingProfileSummary;
+
+public class RoutingProfileService extends BaseService {
+    public Iterable<RoutingProfileSummary> getRoutingProfiles(String userUuid) {
+        // Get the client info
+        ConnectClientInfo clientInfo = getConnectClientInfo(userUuid);
+        // Build the request for the ListRoutingProfiles endpoint using the instanceId
+        ListRoutingProfilesRequest listRoutingProfilesRequest = ListRoutingProfilesRequest.builder()
+                .instanceId(clientInfo.getInstanceId())
+                .build();
+        // The response of the ListRoutingProfiles endpoint
+        ListRoutingProfilesResponse listRoutingProfilesResponse = getConnectClient(clientInfo.getAccessKeyId(),
+        clientInfo.getSecretAccessKey(), clientInfo.getRegion())
+                .listRoutingProfiles(listRoutingProfilesRequest);
+        // The list of Routing Profiles, excluding the nextToken, hopefully the list is not too long.
+        // If it is, we will need to implement pagination.
+        List<RoutingProfileSummary> data = listRoutingProfilesResponse.routingProfileSummaryList();
+        return data;
+    }
+}
--- src/main/java/tc3005b224/amazonconnectinsights/service/SkillService.java
+++ src/main/java/tc3005b224/amazonconnectinsights/service/SkillService.java
@@ -10,8 +10,6 @@ import org.springframework.stereotype.Service;
 
 import software.amazon.awssdk.services.connect.model.DescribeRoutingProfileRequest;
 import software.amazon.awssdk.services.connect.model.DescribeRoutingProfileResponse;
-import software.amazon.awssdk.services.connect.model.ListRoutingProfilesRequest;
-import software.amazon.awssdk.services.connect.model.ListRoutingProfilesResponse;
 import software.amazon.awssdk.services.connect.model.RoutingProfile;
 import software.amazon.awssdk.services.connect.model.RoutingProfileSummary;
 import tc3005b224.amazonconnectinsights.dto.agent.AgentCardDTO;
@@ -24,6 +22,9 @@ import tc3005b224.amazonconnectinsights.models_sql.Alert;
 
 @Service
 public class SkillService extends BaseService {
+    @Autowired
+    private RoutingProfileService routingProfileService;
+        
     @Autowired
     private AlertService alertService;
 
@@ -38,19 +39,9 @@ public class SkillService extends BaseService {
     
     // Service that calls amazon connects ListRoutingProfiles and returns a list of SkillBriefDO
     public List<SkillBriefDTO> findByInstance(String userUuid) {
-        // Get the client info
-        ConnectClientInfo clientInfo = getConnectClientInfo(userUuid);
-        // Build the request for the ListRoutingProfiles endpoint using the instanceId
-        ListRoutingProfilesRequest listRoutingProfilesRequest = ListRoutingProfilesRequest.builder()
-                .instanceId(clientInfo.getInstanceId())
-                .build();
-        // The response of the ListRoutingProfiles endpoint
-        ListRoutingProfilesResponse listRoutingProfilesResponse = getConnectClient(clientInfo.getAccessKeyId(),
-        clientInfo.getSecretAccessKey(), clientInfo.getRegion())
-                .listRoutingProfiles(listRoutingProfilesRequest);
-        // The list of Routing Profiles, excluding the nextToken, hopefully the list is not too long.
-        // If it is, we will need to implement pagination.
-        List<RoutingProfileSummary> data = listRoutingProfilesResponse.routingProfileSummaryList();
+        // Get the list of Routing Profiles
+        Iterable<RoutingProfileSummary> data = routingProfileService.getRoutingProfiles(userUuid);
+        
         // Build the result as a list of SkillBriefDTO
         List<SkillBriefDTO> result = new ArrayList<SkillBriefDTO>();
         data.forEach(routingProfileSummary -> {
