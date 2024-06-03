d033e1aaa2f6fb10a9d935f4457ffd2afdeb3c27
Ian Sánchez <79216621+elyansan@users.noreply.github.com>
2024-06-03 17:33:33 -0600

    Endpoint/queue minimal (#163)
    * Se agrega endpoint QueueMinimal
    
    * Add Set

---
+++ src/main/java/tc3005b224/amazonconnectinsights/dto/queue/QueueMinDTO.java
@@ -0,0 +1,15 @@
+package tc3005b224.amazonconnectinsights.dto.queue;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class QueueMinDTO {
+    private String id;
+    private String name;
+}
