c489f42376171e1a1be12928a8cef7782308f510
FranciscoSanchezz <A01656793@tec.mx>
2024-06-03 20:06:47 -0600

    Refactor de AgentCardDTO

--- src/main/java/tc3005b224/amazonconnectinsights/dto/queue/QueueMinDTO.java
+++ src/main/java/tc3005b224/amazonconnectinsights/dto/queue/QueueMinDTO.java
@@ -12,4 +12,21 @@ import lombok.Setter;
 public class QueueMinDTO {
     private String id;
     private String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
+
--- src/main/java/tc3005b224/amazonconnectinsights/service/AgentService.java
+++ src/main/java/tc3005b224/amazonconnectinsights/service/AgentService.java
@@ -46,6 +46,7 @@ import tc3005b224.amazonconnectinsights.dto.information.ContactInformationDTO;
 import tc3005b224.amazonconnectinsights.dto.information.InformationMetricSectionListDTO;
 import tc3005b224.amazonconnectinsights.dto.information.InformationSectionDTO;
 import tc3005b224.amazonconnectinsights.dto.information.InformationSectionListDTO;
+import tc3005b224.amazonconnectinsights.dto.queue.QueueMinDTO;
 import tc3005b224.amazonconnectinsights.dto.utils.IdAndNameDTO;
 import tc3005b224.amazonconnectinsights.models_sql.Alert;
 
@@ -186,9 +187,9 @@ public class AgentService extends BaseService {
                             .listRoutingProfileQueues(
                                     ListRoutingProfileQueuesRequest.builder().instanceId(clientInfo.getInstanceId())
                                             .routingProfileId(userData.routingProfileId()).build());
-                    Set<String> queuesSet = new HashSet<>();
+                    Set<QueueMinDTO> queuesSet = new HashSet<>();
                     routingProfileQueues.routingProfileQueueConfigSummaryList().forEach(queue -> {
-                        queuesSet.add(queue.queueName());
+                        queuesSet.add(new QueueMinDTO(queue.queueId(), queue.queueName()));
                     });
 
                     // Get the MOST NEGATIVE sentiment status of contacts for the agent
